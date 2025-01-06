d7b8a6de20830aa12aad91191d937751a5791120
eugenewongyj <eugenewong@gmail.com>
2025-01-06 21:40:51 +0800

    3 - Log Levels

--- src/main/java/com/amigoscode/SpringAndSpringBootApplication.java
+++ src/main/java/com/amigoscode/SpringAndSpringBootApplication.java
@@ -1,13 +1,22 @@
 package com.amigoscode;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringAndSpringBootApplication {
 
+	private final static Logger LOGGER = LoggerFactory.getLogger(SpringAndSpringBootApplication.class);
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringAndSpringBootApplication.class, args);
+		LOGGER.info("I am an info message");
+		LOGGER.debug("I am a debug message");
+		LOGGER.warn("I am a warn message");
+		LOGGER.error("I am a error message");
 	}
 
 }
