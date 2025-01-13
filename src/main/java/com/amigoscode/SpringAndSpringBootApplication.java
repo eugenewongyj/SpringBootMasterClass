576ac84770e2a86cda9edd470e15e40b3f05aa9a
eugenewongyj <eugenewong@gmail.com>
2025-01-13 14:38:09 +0800

    after lesson 3 your first api

--- src/main/java/com/amigoscode/SpringAndSpringBootApplication.java
+++ src/main/java/com/amigoscode/SpringAndSpringBootApplication.java
@@ -2,12 +2,37 @@ package com.amigoscode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class SpringAndSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAndSpringBootApplication.class, args);
+	}
+
+	public enum Gender { MALE, FEMALE }
+
+	public record Person(int id, String name, int age, Gender gender) {
+
+	}
+
+	public static List<Person> people = new ArrayList<>();
+
+	static {
+		people.add(new Person(1, "John", 20, Gender.MALE));
+		people.add(new Person(2, "Mariam", 18, Gender.FEMALE));
+		people.add(new Person(3, "Samba", 30, Gender.MALE));
+	}
+
+	@GetMapping
+	public List<Person> getPersons() {
+		return people;
 	}
 
 }
