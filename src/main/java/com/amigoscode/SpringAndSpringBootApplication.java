ee96b3e0a59d19d9ea6dbbe86bfe9a3295cba4b0
eugenewongyj <eugenewong@gmail.com>
2025-01-13 15:24:28 +0800

    after lesson 6 query params

--- src/main/java/com/amigoscode/SpringAndSpringBootApplication.java
+++ src/main/java/com/amigoscode/SpringAndSpringBootApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @RestController
@@ -19,6 +22,8 @@ public class SpringAndSpringBootApplication {
 	}
 
 	public enum Gender { MALE, FEMALE }
+
+	public enum SortOrder { ASC, DESC }
 
 	public record Person(int id, String name, int age, Gender gender) {
 
@@ -33,12 +38,22 @@ public class SpringAndSpringBootApplication {
 	}
 
 	@GetMapping
-	public List<Person> getPersons() {
-		return people;
+	public List<Person> getPersons(@RequestParam(value = "sort", required = false, defaultValue = "ASC") SortOrder sort,
+								   @RequestParam(value = "limit", required = false, defaultValue = "10") Integer limit) {
+		if (sort == SortOrder.ASC) {
+			return people.stream()
+					.sorted(Comparator.comparing(Person::id))
+					.collect(Collectors.toList());
+		}
+		return people.stream()
+				.sorted(Comparator.comparing(Person::id).reversed())
+				.collect(Collectors.toList());
 	}
 
 	@GetMapping("/{id}")
-	public Optional<Person> getPersonById(@PathVariable("id") Integer id) {
+	public Optional<Person> getPersonById(@PathVariable("id") Integer id,
+										  @RequestParam(value = "sort", required = false, defaultValue = "ASC") SortOrder sort) {
+		System.out.println(sort);
 		return people.stream().filter(person -> person.id == id).findFirst();
 	}
 
